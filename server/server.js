32eea4b3c0c1f75a07705fda39d78b8b8ce43691
Conan Browne <Ventrik101@Gmail.com>
2021-03-15 22:47:25 -0400

    Refactoring

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const logger = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const errorHandler = require('errorhandler')
@@ -8,6 +10,7 @@ const path = require('path')
 const session = require('express-session')
 
 console.log('Hello from the start of the server')
+
 // Set-up Mongo
 const MONGO_URI = process.env.MONGO_URI
 const PORT = process.env.PORT || 5000
@@ -59,11 +62,15 @@ mongoose
 	.catch((err) => console.error('Error connecting to mongo', err))
 
 // Add our Models and Routes
-// app.use(require('./routes'))
 const postsRouter = require('./routes/post')
 const usersRouter = require('./routes/users')
 const commentsRouter = require('./routes/comment')
 const notificationRouter = require('./routes/notification')
+app.use('/api/post/', postsRouter)
+app.use('/api/users/', usersRouter)
+app.use('/api/comment/', commentsRouter)
+app.use('/api/notification/', notificationRouter)
+
 require('./models/Comment')
 require('./models/CommentLike')
 require('./models/CommentReply')
@@ -75,42 +82,10 @@ require('./models/Post')
 require('./models/PostLike')
 require('./models/Users')
 require('./config/passport')
-app.use('/api/post/', postsRouter)
-app.use('/api/users/', usersRouter)
-app.use('/api/comment/', commentsRouter)
-app.use('/api/notification/', notificationRouter)
-
-// Errors and Middleware
-// if (!isProduction) {
-// 	app.use((err, req, res) => {
-// 		res.status(err.status || 500)
-// 		res.json({
-// 			errors: {
-// 				message: err.message,
-// 				error: err,
-// 			},
-// 		})
-// 	})
-// }
-
-// app.use((err, req, res) => {
-// 	res.status(err.status || 500)
-// 	res.json({
-// 		errors: {
-// 			message: err.message,
-// 			error: {},
-// 		},
-// 	})
-// })
 
 app.get('/auth/reset/password/:jwt', function (req, res) {
 	return res.status(404).json({ message: 'go to port 3000' })
 })
-
-// Make the Server display Client.
-// app.get('*', (req, res, next) => {
-// 	res.sendFile(path.join(__dirname, '../client/public/index.html'))
-// })
 
 app.get('*', (req, res, next) => {
 	res.sendFile(path.join(__dirname, '../client/build/index.html'))
