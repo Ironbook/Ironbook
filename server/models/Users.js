0ec6a3877eae282ea22267f4a2bf7737248a745c
Conan Browne <Ventrik101@Gmail.com>
2021-03-15 21:44:33 -0400

    Refactoring

--- server/models/Users.js
+++ server/models/Users.js
@@ -59,7 +59,7 @@ const UsersSchema = new mongoose.Schema({
 		required: true,
 		maxlength: 40,
 		unique: true,
-		match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+		match: /[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?/,
 	},
 	profilePicture: {
 		type: String,
